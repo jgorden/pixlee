94ff97432e28a15cd082c2d5418e6fc8016d2776
jgorden <joji.gorden@gmail.com>
2016-05-13 14:11:37 -0700

    add how route

--- insta/app/controllers/collections_controller.rb
+++ insta/app/controllers/collections_controller.rb
@@ -1,20 +1,23 @@
 class CollectionsController < ApplicationController
 
     def index
-      # uri = URI.parse("https://api.instagram.com/v1/tags/cats/media/recent?access_token=174180622.1677ed0.f86a491bf33e4f41bad53c20f480a7a0")
-      # http = Net::HTTP.new(uri.host, uri.port)
-      # http.use_ssl = true
-      # http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      # request = Net::HTTP::Get.new(uri.request_uri)
-      # response = http.request(request)
-      # render json: response.body
-      render json: { woo: 'weeeee' }
+      all = Collection.all
+      render json: all
     end
 
     def create
-      p params
       col = Collection.create(collection_params)
       render json: col
+    end
+
+    def show
+      uri = URI.parse("https://api.instagram.com/v1/tags/cats/media/recent?access_token=174180622.1677ed0.f86a491bf33e4f41bad53c20f480a7a0")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      request = Net::HTTP::Get.new(uri.request_uri)
+      response = http.request(request)
+      render json: response.body
     end
 
     private
