3d19d01f9f934f735a6d578c07e3a83253fbdfb0
jgorden <joji.gorden@gmail.com>
2016-05-13 12:50:56 -0700

    rack cors/API code

--- insta/config/application.rb
+++ insta/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Insta
   class Application < Rails::Application
+    config.api_only = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -22,5 +23,11 @@ module Insta
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.insert_before 0, 'Rack::Cors' do
+        allow do
+            origins '*'
+            resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options, :patch, :head]
+        end
+    end
   end
 end
