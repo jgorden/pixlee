503e3bd953d4007628596beff58ad395e90500c9
jgorden <joji.gorden@gmail.com>
2016-05-16 12:57:29 -0700

    styles/positioning on individual posts

--- public/js/filters/media.js
+++ public/js/filters/media.js
@@ -1,9 +1,9 @@
 angular.module('pic')
   .filter("media", ['$sce', function($sce) {
     return function(link){
-      if (link.includes('.jpg')){ tag = "<img ng-src='" + link + "' class='ng-scope' " + "src='" + link +  "'>" }
+      if (link.includes('.jpg')){ tag = "<img ng-src='" + link + "' class='center-block' " + "src='" + link +  "'>" }
       else if(link.includes('.mp4')){ 
-        tag = "<video width='640' height='480' controls><source src=" + link + " type='video/mp4'></video>"
+        tag = "<video width='640' height='480' controls class='center-block'><source src=" + link + " type='video/mp4'></video>"
       }
       return $sce.trustAsHtml(tag);
     }
